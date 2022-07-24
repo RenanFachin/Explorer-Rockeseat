669385e6aa0d9dbf0d6284231e95d33c06d4375d
Renan Fachin <82840836+RenanFachin@users.noreply.github.com>
2022-07-24 17:43:14 -0300

    Add files via upload
    Html e css finalizados.
    Inicio do arquivo Javascript. 
    onclick="xxxx()"
    .querySelector
    .value
    .preventDefault

---
+++ STAGE 05/Jogodeadivinhacao/js/main.js
@@ -0,0 +1,6 @@
+function handleClick(event){
+    event.preventDefault()
+
+    const input = document.querySelector('#inputNumber') // Pegando a id inputNumber do HTML
+    console.log(input.value) // Pegando o "valor" do formulário input
+}
