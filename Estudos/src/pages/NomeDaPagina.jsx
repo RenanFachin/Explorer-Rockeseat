6d9937a72f7a3a8262bb912fbc96ea97a7bff87c
Renan Fachin <82840836+RenanFachin@users.noreply.github.com>
2022-09-02 15:18:27 -0300

    Delete Estudos/src directory

--- Estudos/src/pages/NomeDaPagina.jsx
+++
@@ -1,6 +0,0 @@
-export function NomeDaPagina(){
-  return(
-    // Aqui é retornado toda a página;
-    <h2>Olá mundo!!!</h2>
-  )
-}
--- Estudos/src/styles/theme.js
+++
@@ -1,6 +0,0 @@
-export default {
-    COLORS: {
-      // Cores de fundo
-      BACKGROUND_COLOR_900: "#1C1B1E"
-    }
-}
